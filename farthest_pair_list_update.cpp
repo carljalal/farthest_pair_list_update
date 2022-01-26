f1478cd73b246e87206fad6d9e25d2b2f2f81eac
Carl Jalal <carljalal@gmail.com>
2022-01-25 21:31:46 -0500

    Add project files.

---
+++ farthest_pair_list_update.cpp
@@ -0,0 +1,36 @@
+// directories
+// storages
+// organization of words and id's
+
+#include "iostream"
+#include "Windows.h"
+#include "vector"
+#include "conio.h"
+#include "string"
+#include "fstream"
+
+
+// read file or text
+// vectorize
+// have id's
+// parsing by line, by delimiter, into arrays
+
+
+// interactive loop
+// automatic (optional) process
+
+// get interrupt key (optional)
+// ask for input
+
+// save results
+
+
+// peaceful and kindness and understanding interactions
+// honest and kindness and understanding feedback interaction communications
+// togetherness
+int main()
+{
+    
+}
+
+
